7c315d337fe8153431db9da4132a2913d10a6127
stephen.liu <stephen.liu.fipo@gmail.com>
2024-07-09 14:59:02 +0800

    fix workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "r3f-drei-sticker-demo",
+  base: "/r3f-drei-sticker-demo",
 });
